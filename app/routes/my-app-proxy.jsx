334fd36c2f07aa9acc462a772897f0ae1052e511
wc-rahul <wc.rahulmeghani@gmail.com>
2026-02-06 10:58:57 +0530

    newor

--- app/routes/my-app-proxy.jsx
+++ app/routes/my-app-proxy.jsx
@@ -1,22 +1,27 @@
-import { authenticate } from "../shopify.server";
-import { useLoaderData } from "react-router";
 
-export const loader = async ({ request }) => {
-  // Use the authentication API from the React Router template
-  console.log('RRRRRAAAAAAAHHHHHHHUUUUUULLLLLLL',request)
-  await authenticate.public.appProxy(request);
+import { useLoaderData } from 'react-router';
+import {authenticate} from '../shopify.server';
 
-  // Read URL parameters added by Shopify when proxying
-  const url = new URL(request.url);
+export const loader = async ({request}) => {
+  const {storefront, liquid} = await authenticate.public.appProxy(request);
 
-  return {
-    shop: url.searchParams.get("shop"),
-    loggedInCustomerId: url.searchParams.get("logged_in_customer_id"),
-  };
+  if (!storefront) {
+    return new Response();
+  }
+
+  const response = await storefront.graphql(
+    `#graphql
+    query productTitle {
+      products(first: 1) {
+        nodes {
+          title
+        }
+      }
+    }`,
+  );
+  const body = await response.json();
+
+  const title = body.data.products.nodes[0].title;
+
+  return liquid(`Found product ${title} from {{shop.name}}`);
 };
-
-export default function MyAppProxy() {
-  const { shop, loggedInCustomerId } = useLoaderData();
-
-  return <div>{`Hello world from ${loggedInCustomerId || "not-logged-in"} on ${shop}`}</div>;
-}
