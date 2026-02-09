8e241d7ccc829b3a8ebf5ca98f3476868d5a7364
wc-rahul <wc.rahulmeghani@gmail.com>
2026-02-09 12:23:40 +0530

    app update with cart extension 5

--- app/firebase.server.js
+++ app/firebase.server.js
@@ -3,9 +3,9 @@ import admin from "firebase-admin";
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+            project_id: process.env.FIREBASE_PROJECT_ID,
+            client_email: process.env.FIREBASE_CLIENT_EMAIL,
+            private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
         }),
     });
 }
